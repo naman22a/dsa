a93185ee4ef16701f6d2a27835aa700aa2b3f51d
Naman Arora <71423632+naman22a@users.noreply.github.com>
2026-06-22 18:57:46 +0530

    Refactor 121 ninjaTraining_space_optimization

--- 26-dp/121.cpp
+++ 26-dp/121.cpp
@@ -92,30 +92,32 @@ int ninjaTraining_space_optimization(int n, vector<vector<int>> &points)
 {
     vector<int> prev(4, 0);
 
-    // base cases
-    prev[0] = max(points[0][1], points[0][2]);
-    prev[1] = max(points[0][0], points[0][2]);
-    prev[2] = max(points[0][0], points[0][1]);
-    prev[3] = max(points[0][0], max(points[0][1], points[0][2]));
+    // base case
+    for (int lastTask = 0; lastTask < 4; lastTask++)
+    {
+        int maxi = 0;
+        for (int task = 0; task <= 2; task++)
+        {
+            if (task != lastTask)
+                maxi = max(maxi, points[0][task]);
+        }
+        prev[lastTask] = maxi;
+    }
 
     for (int day = 1; day <= n - 1; day++)
     {
-        vector<int> temp(4, 0);
-        for (int last = 0; last < 4; last++)
+        vector<int> cur(4, 0);
+        for (int lastTask = 0; lastTask < 4; lastTask++)
         {
-            temp[last] = 0;
-
+            int maxi = 0;
             for (int task = 0; task <= 2; task++)
             {
-                if (task != last)
-                {
-                    int point = points[day][task] + prev[task];
-                    temp[last] = max(temp[last], point);
-                }
+                if (task != lastTask)
+                    maxi = max(maxi, points[day][task] + prev[task]);
             }
+            cur[lastTask] = maxi;
         }
-
-        prev = temp;
+        prev = cur;
     }
 
     return prev[3];
